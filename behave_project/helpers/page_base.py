cde1c7914c21b9f606f44653cf63cd0a0331b5b0
Reinaldo Rossetti <reiload@gmail.com>
2017-05-01 18:33:04 -0300

    Alterando a PageBase.

--- behave_project/helpers/page_base.py
+++ behave_project/helpers/page_base.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
 
 class PageBase:
 
@@ -11,8 +12,10 @@ class PageBase:
 
     def wait_(self, locator):
         locator = locator.split("=", 1)
-        self.wait.until(lambda driver: driver.find_element(locator[0], locator[1]))
+        element = self.wait.until(visibility_of_element_located(locator))
+        return element
 
     def wait_not(self, locator):
         locator = locator.split("=", 1)
-        self.wait.until_not(lambda driver: driver.find_element(locator[0], locator[1]))
+        element = self.wait.until_not(visibility_of_element_located(locator))
+        return element
